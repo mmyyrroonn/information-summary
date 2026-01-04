a0eb81e967a116f55d6b2a0b7f378256d0a9ad16
mmyyrroonn <qiufan_glamour@hotmail.com>
2026-01-04 11:52:44 +0800

    Increase classify concurrency default

--- server/src/config.ts
+++ server/src/config.ts
@@ -27,7 +27,7 @@ const envSchema = z.object({
   FETCH_BATCH_SIZE: z.coerce.number().default(10),
   FETCH_COOLDOWN_HOURS: z.coerce.number().default(12),
   CLASSIFY_MIN_TWEETS: z.coerce.number().default(10),
-  CLASSIFY_CONCURRENCY: z.coerce.number().default(4),
+  CLASSIFY_CONCURRENCY: z.coerce.number().default(6),
   REPORT_TIMEZONE: z.string().default('Asia/Shanghai'),
   BASE_WEB_URL: z.string().default('http://localhost:5173'),
   AI_LOCK_TTL_MS: z.coerce.number().default(60 * 60 * 1000),
